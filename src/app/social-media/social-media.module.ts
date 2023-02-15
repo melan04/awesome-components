634ea033254bda11433db783a6a4c3448680b6a4
Mélany Condé <melany.conde@gmail.com>
2023-02-15 20:25:43 +0100

    install services

--- src/app/social-media/social-media.module.ts
+++ src/app/social-media/social-media.module.ts
@@ -3,16 +3,21 @@ import { CommonModule } from '@angular/common';
 
 import { SocialMediaRoutingModule } from './social-media-routing.module';
 import { PostsService } from './services/posts.service';
+import { PostsResolver } from './resolvers/posts.resolver';
+import { PostListComponent } from './components/post-list/post-list.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    PostListComponent
+  ],
   imports: [
     CommonModule,
     SocialMediaRoutingModule
   ],
   providers: [
-    PostsService
+    PostsService,
+    PostsResolver
   ]
 })
 export class SocialMediaModule { }
