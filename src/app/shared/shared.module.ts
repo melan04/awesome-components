4ee9ae6e2ae3576e4de3c30573911c4d44905149
Mélany Condé <melany.conde@gmail.com>
2023-02-22 08:35:25 +0100

    add components

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
   declarations: [],
   imports: [CommonModule],
-  exports: [MatToolbarModule],
+  exports: [MatToolbarModule, MatCardModule],
 })
 export class SharedModule {}
