b085337cb779fd41ae88ffd1686dd06dd3ff1c0a
Condé <melany.conde@accenture.com>
2023-03-30 18:00:21 +0200

    animation complexe

--- src/app/shared/components/comments/comments.component.ts
+++ src/app/shared/components/comments/comments.component.ts
@@ -1,8 +1,15 @@
-import { animate, state, style, transition, trigger } from '@angular/animations';
+import {
+  animate,
+  group,
+  query,
+  state,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { FormBuilder, FormControl, Validators } from '@angular/forms';
 import { Comment } from 'src/app/core/models/comment.model';
-
 
 @Component({
   selector: 'app-comments',
@@ -10,76 +17,105 @@ import { Comment } from 'src/app/core/models/comment.model';
   styleUrls: ['./comments.component.scss'],
   animations: [
     trigger('listItem', [
-      state('default', style({
-        transform: 'scale(1)',
-        'background-color': 'white',
-        'z-index': 1
-      })),
-      state('active', style({
-        transform: 'scale(1.05)',
-        'background-color': 'rgb(201, 157, 242)',
-        'z-index': 2
-      })),
-      transition('default => active', [
-        animate('100ms ease-in-out')
-      ]),
-      transition('active => default', [
-        animate('500ms ease-in-out')
-      ]),
-      transition('void => *', [
+      state(
+        'default',
         style({
-            transform: 'translateX(-100%)',
+          transform: 'scale(1)',
+          'background-color': 'white',
+          'z-index': 1,
+        })
+      ),
+      state(
+        'active',
+        style({
+          transform: 'scale(1.05)',
+          'background-color': 'rgb(201, 157, 242)',
+          'z-index': 2,
+        })
+      ),
+      transition('default => active', [animate('100ms ease-in-out')]),
+      transition('active => default', [animate('500ms ease-in-out')]),
+      transition(':enter', [
+        query('span', [
+          style({
             opacity: 0,
-            'background-color': 'rgb(201, 157, 242)',
+          }),
+        ]),
+        style({
+          transform: 'translateX(-100%)',
+          opacity: 0,
+          'background-color': 'rgb(201, 157, 242)',
         }),
-        animate('250ms ease-out', style({
+        animate(
+          '250ms ease-out',
+          style({
             transform: 'translateX(0)',
             opacity: 1,
             'background-color': 'white',
-        }))
-    ])
-    ])
-  ]
+          })
+        ),
+        group([
+          query('.comment-text', [
+            animate(
+              '250ms',
+              style({
+                opacity: 1,
+              })
+            ),
+          ]),
+          query('.comment-date', [
+            animate(
+              '500ms',
+              style({
+                opacity: 1,
+              })
+            ),
+          ]),
+        ]),
+      ]),
+    ]),
+  ],
 })
 export class CommentsComponent implements OnInit {
+  @Input() comments!: Comment[];
+  @Output() newComment = new EventEmitter<string>();
 
-@Input() comments!: Comment[];
-@Output() newComment = new EventEmitter<string>();
-
- 
   commentCtrl!: FormControl;
   animationStates: { [key: number]: 'default' | 'active' } = {};
   listItemAnimationState: 'default' | 'active' = 'default';
- 
+
   constructor(private formBuilder: FormBuilder) {}
 
-  ngOnInit(): void { 
-    this.commentCtrl = this.formBuilder.control('', [Validators.required, Validators.minLength(10)])
+  ngOnInit(): void {
+    this.commentCtrl = this.formBuilder.control('', [
+      Validators.required,
+      Validators.minLength(10),
+    ]);
     for (let index in this.comments) {
       this.animationStates[index] = 'default';
-  }
+    }
   }
 
   onLeaveComment() {
     if (this.commentCtrl.invalid) {
-        return;
+      return;
     }
-    const maxId = Math.max(...this.comments.map(comment => comment.id));
+    const maxId = Math.max(...this.comments.map((comment) => comment.id));
     this.comments.unshift({
-        id: maxId + 1,
-        comment: this.commentCtrl.value,
-        createdDate: new Date().toISOString(),
-        userId: 1
+      id: maxId + 1,
+      comment: this.commentCtrl.value,
+      createdDate: new Date().toISOString(),
+      userId: 1,
     });
     this.newComment.emit(this.commentCtrl.value);
     this.commentCtrl.reset();
-}
+  }
 
   onListItemMouseEnter(index: number) {
     this.animationStates[index] = 'active';
-}
+  }
 
-onListItemMouseLeave(index: number) {
+  onListItemMouseLeave(index: number) {
     this.animationStates[index] = 'default';
-}
+  }
 }
