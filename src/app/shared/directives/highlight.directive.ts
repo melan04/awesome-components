7c511635b34c82fe111881b7f94ac560b69ff81a
Condé <melany.conde@accenture.com>
2023-03-30 11:40:56 +0200

    add Hostlistener

--- src/app/shared/directives/highlight.directive.ts
+++ src/app/shared/directives/highlight.directive.ts
@@ -1,20 +1,32 @@
-import { AfterViewInit, Directive, ElementRef, Input, Renderer2 } from '@angular/core';
+import { AfterViewInit, Directive, ElementRef, HostListener, Input, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[highlight]'
 })
 export class HighlightDirective implements AfterViewInit {
 
-    @Input() highlight!: string;
+  @Input() color = 'yellow';
 
   constructor(private el: ElementRef,
               private renderer: Renderer2) {}
 
   ngAfterViewInit() {
-    this.setBackgroundColor(this.highlight);
+    this.setBackgroundColor(this.color);
   }
 
   setBackgroundColor(color: string) {
     this.renderer.setStyle(this.el.nativeElement, 'background-color', color);
+  }
+
+  @HostListener('mouseenter') onMouseEnter() {
+    this.setBackgroundColor('lightgreen');
+  }
+
+  @HostListener('mouseleave') onMouseLeave() {
+    this.setBackgroundColor(this.color);
+  }
+
+  @HostListener('click') onClick() {
+    this.color = 'lightgreen';
   }
 }
