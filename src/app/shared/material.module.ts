678b6e41b10b5c4fc590e42812face254c90cd6f
Condé <melany.conde@accenture.com>
2023-04-05 17:22:36 +0200

    add filter

--- src/app/shared/material.module.ts
+++ src/app/shared/material.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from "@angular/core";
-import { MatCardModule } from "@angular/material/card";
 import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatCardModule } from "@angular/material/card";
 import { MatListModule } from '@angular/material/list';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
@@ -9,20 +9,23 @@ import { MatInputModule } from '@angular/material/input';
 import {MatCheckboxModule} from '@angular/material/checkbox';
 import {MatRadioModule} from '@angular/material/radio';
 import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {MatSelectModule} from '@angular/material/select'
 
 
 @NgModule({
     exports: [
-    MatToolbarModule,
-    MatCardModule,
-    MatListModule,
-    MatButtonModule,
-    MatIconModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatCheckboxModule,
-    MatRadioModule,
-    MatProgressSpinnerModule,
+      MatToolbarModule,
+      MatCardModule,
+      MatListModule,
+      MatButtonModule,
+      MatIconModule,
+      MatFormFieldModule,
+      MatInputModule,
+      MatCheckboxModule,
+      MatRadioModule,
+      MatProgressSpinnerModule,
+      MatSelectModule
     ]
-    })
-    export class MaterialModule {}
+  })
+  export class MaterialModule {}
+  
